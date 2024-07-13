0cf574c3367d24ffa5fd58968946ef99997a5080
JHJ08 <jhj970811@gmail.com>
2024-07-12 22:56:48 -0700

    2024_07_12 주석 수정

--- Algo-3Team/src/week_1/JHJ08/PGS_H_Index.java
+++ Algo-3Team/src/week_1/JHJ08/PGS_H_Index.java
@@ -11,15 +11,15 @@ public class PGS_H_Index {
         Arrays.sort(citations); // 정렬 해주기 
         
         // 정렬 => [0, 1, 3, 5, 6]
-        // 0번 이상 인용된 논문 갯수 : citations.length(=5)
-        // 1번 이상 인용된 논문 갯수 : citations.length(=5) - 1 = 4
-        // 3번 이상 인용된 논문 갯수 : citations.length(=5) - 2 = 3
-        // 5번 이상 인용된 논문 갯수 : citations.length(=5) - 3 = 2
-        // 6번 이상 인용된 논문 갯수 : citations.length(=5) - 4 = 1
+        // 0번 이상 인용된 논문 수 : citations.length(=5)
+        // 1번 이상 인용된 논문 수 : citations.length(=5) - 1 = 4
+        // 3번 이상 인용된 논문 수 : citations.length(=5) - 2 = 3
+        // 5번 이상 인용된 논문 수 : citations.length(=5) - 3 = 2
+        // 6번 이상 인용된 논문 수 : citations.length(=5) - 4 = 1
         
         for(int i=0; i<citations.length; i++)
         {
-            int h = citations.length - i; // 논문 갯수
+            int h = citations.length - i; // 논문 개수 
             
             if(citations[i] >= h) //h번 이상 인용된 논문이 h편 이상인 경우 확인
             {
